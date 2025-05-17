7264637f866087ed6eab1346b9f2fdb71d09779b
Doit <duhee7524@naver.com>
2025-05-17 17:08:44 +0900

    project setup: React + TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '.vscode',
+      'public',
+      'package-lock.json',
+      'pnpm-lock.yaml',
+    ],
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          tabWidth: 2,
+          trailingComma: 'es5',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 'warn', // 사용하지 않는 변수 경고
+      'react/react-in-jsx-scope': 'off',
+    },
+  }
+);

aa7e76403a746c42100aff17d9a4a90d8296fc44
kich555 <hyunjjang199582@gmail.com>
2025-05-29 12:05:34 +0900

    feat: 쿼리키 팩토리 추가 및 훅 개선 - 쿼리키 팩토리(queryKeys) 추가하여 코드 재사용성 향상 - usePopularMovies, usePopularMoviesInfinite, useSearchMoviesInfinite 훅에서 쿼리키 팩토리 사용 - 코드 가독성 및 유지보수성 개선

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,42 +1,63 @@
 import {
   useSuspenseInfiniteQuery,
   useSuspenseQuery,
+  queryOptions,
+  infiniteQueryOptions,
 } from '@tanstack/react-query';
 import { getPopularMovies, getSearchMovies } from '@/services/movieApi';
-import { GetSearchMoviesParams, SearchMovieResponse } from '@/types/movie';
+import { GetSearchMoviesParams, SearchMovieResponse, GetMoviesParams } from '@/types/movie';
 
-export const usePopularMovies = ({ page = 1, language = 'en-US' }) => {
-  return useSuspenseQuery({
-    queryKey: ['popularMovies', { page, language }],
-    queryFn: () => getPopularMovies({ page, language }),
-  });
+// 쿼리키 팩토리 정의
+export const queryKeys = {
+  movie: {
+    all: ['movie'] as const,
+    popular: (params: GetMoviesParams) =>
+      queryOptions({
+        queryKey: [...queryKeys.movie.all, 'popular', params],
+        queryFn: () => getPopularMovies(params),
+      }),
+    popularInfinite: (language = 'en-US') =>
+      infiniteQueryOptions({
+        queryKey: [...queryKeys.movie.all, 'popular-infinite', language],
+        queryFn: ({ pageParam = 1 }) =>
+          getPopularMovies({ page: pageParam, language }),
+        initialPageParam: 1,
+        getNextPageParam: (lastPage) => {
+          const next = lastPage.page + 1;
+          return next <= lastPage.total_pages ? next : undefined;
+        },
+      }),
+    search: (params: GetSearchMoviesParams) =>
+      queryOptions({
+        queryKey: [...queryKeys.movie.all, 'search', params],
+        queryFn: () => getSearchMovies(params),
+      }),
+    searchInfinite: (params: Pick<GetSearchMoviesParams, 'query' | 'language'>) =>
+      infiniteQueryOptions<SearchMovieResponse, Error>({
+        queryKey: [...queryKeys.movie.all, 'search-infinite', params],
+        queryFn: ({ pageParam = 1 }) =>
+          getSearchMovies({ ...params, page: pageParam as number }),
+        initialPageParam: 1,
+        getNextPageParam: (lastPage) => {
+          const next = lastPage.page + 1;
+          return next <= lastPage.total_pages ? next : undefined;
+        },
+      }),
+  },
+};
+
+// 개선된 훅들 - queryKeys 팩토리 사용
+export const usePopularMovies = ({ page = 1, language = 'en-US' }: GetMoviesParams = {}) => {
+  return useSuspenseQuery(queryKeys.movie.popular({ page, language }));
 };
 
 export const usePopularMoviesInfinite = (language = 'en-US') => {
-  return useSuspenseInfiniteQuery({
-    queryKey: ['popular-movies', language],
-    queryFn: ({ pageParam = 1 }) =>
-      getPopularMovies({ page: pageParam, language }),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage) => {
-      const next = lastPage.page + 1;
-      return next <= lastPage.total_pages ? next : undefined;
-    },
-  });
+  return useSuspenseInfiniteQuery(queryKeys.movie.popularInfinite(language));
 };
 
 export const useSearchMoviesInfinite = ({
   query,
   language = 'en-US',
 }: GetSearchMoviesParams) => {
-  return useSuspenseInfiniteQuery<SearchMovieResponse, Error>({
-    queryKey: ['search-movies', { query, language }],
-    queryFn: ({ pageParam = 1 }) =>
-      getSearchMovies({ query, language, page: pageParam as number }),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage) => {
-      const next = lastPage.page + 1;
-      return next <= lastPage.total_pages ? next : undefined;
-    },
-  });
+  return useSuspenseInfiniteQuery(queryKeys.movie.searchInfinite({ query, language }));
 };
